21b340b4bef60d9d5c6a1b92493791bd19550b83
JoseAmaliya <joseamaliyaambarwati2@gmail.com>
2026-06-18 19:43:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -103,5 +103,28 @@ document.body.style.color =
 "white";
 
 }
+}
+
+function searchMenu(){
+
+let input =
+document.getElementById("searchMenu")
+.value.toLowerCase();
+
+let cards =
+document.querySelectorAll(".produk-card");
+
+cards.forEach(card => {
+
+let text =
+card.innerText.toLowerCase();
+
+if(text.includes(input)){
+card.style.display = "block";
+}else{
+card.style.display = "none";
+}
+
+});
 
 }
